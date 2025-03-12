aed957ed32cb03e4de861b8c5d08842173ac5782
Roberto Kalinovskyy <roberto.kalinovskyy@gmail.com>
2025-03-12 22:32:47 +0100

    fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from .constants import projects
+from src.constants import projects
 
 app = FastAPI()
 
